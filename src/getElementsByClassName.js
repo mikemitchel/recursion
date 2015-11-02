8451566e9903559dd3b99dd1470bd5c575c47b13
MKS-students <mks.students@hackreactor.com>
2015-11-02 12:32:22 -0600

    maybe we are close to recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,28 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-){
-  // your code here
+var getElementsByClassName = function(className){
+  var parent= document.body;
+  var results = [];
+
+
+  var test = function(node) {
+    return node !== undefined && node !== null;
+  }
+
+
+  var getEm = function (parent) {
+    if (parent.classList.contains(className)) {
+      results.push(parent)
+    }
+    if (test(parent.nextSibling)) {
+      getEm(parent.nextSibling);
+    }
+    if (test(parent.firstChild)){
+      getEm(parent.firstChild)
+    }
+  }
+
+  return results;
+
 };
